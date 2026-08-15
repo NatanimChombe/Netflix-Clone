b553ed0c090015828bf966154787ae4f9c456954
NatanimChombe <natanimchombe6@gmail.com>
2026-08-15 15:09:14 +0300

    heade and footer

---
+++ netflix-clone/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Componet/Header/Header'
+import Footer from '../Componet/Footer/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
