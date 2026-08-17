96a7ef0c99170afe1dadc90b9921bc420bfe962f
NatanimChombe <natanimchombe6@gmail.com>
2026-08-17 20:07:38 +0300

    Trailer

--- netflix-clone/src/Componet/Row/Row/Row.jsx
+++ netflix-clone/src/Componet/Row/Row/Row.jsx
@@ -1,14 +1,13 @@
 import React,{useEffect,useState}  from 'react'
 import './row.css'
 import axios from '../../../utils/axios'
-// import movieTrailer from 'movie trailer'
-// import YouTube  from 'react-youtube';
+import movieTrailer from 'movie-trailer'
+import YouTube  from 'react-youtube';
 
 
 function Row({title,fetchUrl,isLargeRow}) {
-   // console.log("ALL PROPS:", {title, fetchUrl, isLargeRow});
     const [movie,setMovie] = useState([]);
-    // const [trailerUrl,setTrailerUrl] = useState("");
+    const [trailerUrl,setTrailerUrl] = useState("");
     const base_url = "https://image.tmdb.org/t/p/original"
 
 
@@ -25,20 +24,20 @@ function Row({title,fetchUrl,isLargeRow}) {
     })();
 },[fetchUrl]);
 
-// const handleClick =(movie) =>{
-//     if(trailerUrl){
-//         setTrailerUrl('')
-//     }else {
-//         movieTrailer(movie?.title  || movie?.name || movie?.original_name)
-//         .then ((url )=> {
-//             console.log(url)
-//             const urlParams=new URLSearchParams(new URL(url).search)
-//             console.log( urlParams)
-//             console.log( urlParams.get('v'))
-//             setTrailerUrl(urlParams.get('v'));
-//         })
-//     }
-// }
+const handleClick =(movie) =>{
+    if(trailerUrl){
+        setTrailerUrl('')
+    }else {
+        movieTrailer(movie?.title  || movie?.name || movie?.original_name)
+        .then ((url )=> {
+            console.log(url)
+            const urlParams=new URLSearchParams(new URL(url).search)
+            console.log( urlParams)
+            console.log( urlParams.get('v'))
+            setTrailerUrl(urlParams.get('v'));
+        })
+    }
+}
 const opts ={
     height:'300',
     width:"100%",
@@ -56,18 +55,16 @@ return (
                 {movie?.map((movie,index)=>{
                     return(
                     <img 
-                        // onClick={()=>handleClick(movie)  }
+                        onClick={()=>handleClick(movie)  }
                         key={index} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}
-                        // className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                         className={isLargeRow ? "row_poster row_posterLarge" : "row_poster"}
-                        //className="row_posterLarge"
                     /> 
                     ) 
                 })}
             </div>
-            {/* <div style={{padding:'40px'}} >
+            <div style={{padding:'40px'}} >
                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />  }
-            </div> */}
+            </div>
         </div>
     </>
 )
